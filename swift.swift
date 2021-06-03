90da6289b54d8189cd533e40f1495a7eb86dc731
pwang86 <pwang86@gmail.com>
2021-06-03 13:49:44 +1000

    binary search in swift

--- swift.swift
+++ swift.swift
@@ -9,4 +9,20 @@ class Solution {
         }
         return -1
     }
+
+    // binary search (items in array are unique and arr is sorted)
+    func binarySearch(_ arr: [Int], _ item: Int) -> Int {
+        var l = 0
+        var r = arr.count - 1
+        while (l <= r) {
+            let mid = (l + r) / 2
+            if (arr[mid] == item) 
+                return mid
+            else if (arr[mid] > item)
+                r = mid - 1
+            else 
+                l = mid + 1
+        }
+        return -1
+    }
 }
