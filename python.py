65d33b0251c18114db389bb00543dd6a99de547b
pwang86 <pwang86@gmail.com>
2021-06-03 13:49:27 +1000

    binary search in python

--- python.py
+++ python.py
@@ -6,3 +6,16 @@ class Solution:
             if arr[i] == item:
                 return i
         return -1
+
+    #binary search (sorted array)
+    def binarySearch(self, arr: List[int], item: int) -> int:
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if arr[mid] == item:
+                return mid
+            elif arr[mid] > item:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
