8d422fd51ad4464551c025518a39b35c688a0b6f
pwang86 <pwang86@gmail.com>
2021-06-03 13:48:28 +1000

    binary search in js

--- javascript.js
+++ javascript.js
@@ -11,3 +11,22 @@ const linearSearch = (arr, item) => {
   }
   return -1;
 };
+
+// binary search (sorted array)
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number} item 
+ * @returns {number} 
+ */
+const binarySearch = (arr, item) => {
+  let l = 0, r = arr.length - 1;
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+    const guess = arr[mid];
+    if (guess == item) return mid;
+    if (guess > item) r = mid - 1;
+    else l = mid + 1;
+  }
+  return -1;
+};
